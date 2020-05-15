307c82ba53eaf336544a6a63e21f5996c34abfd4
Jamesley Joseph <jamesleyjoseph5@gmail.com>
2020-05-15 00:13:47 -0400

    change me query

--- packages/server/src/modules/Auth/me/resolvers.ts
+++ packages/server/src/modules/Auth/me/resolvers.ts
@@ -1,10 +1,19 @@
 import { User } from "../../../entity/User";
 import { IResolvers } from "apollo-server-express";
+import { Service } from "../../../entity/service";
 
 export const Me: IResolvers = {
   Query: {
-    me: (_, __, { session }) => {
-      return User.findOne({ where: { id: session.userId } });
+    me: async (_, __, { session }) => {
+      const user = await User.findOne({ where: { id: session.userId } });
+      const userId = user?.id;
+      const service = await Service.find({ where: { ownerId: userId } });
+
+      const userService = {
+        user,
+        service,
+      };
+      return userService;
     },
   },
 };
