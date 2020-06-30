e193720d9e8d4c7c79238c05c64cace99cb92535
Jamesley <Jamesleyjoseph5@gmail.com>
2020-06-30 17:16:44 -0400

    new Mac

--- packages/mobil/src/apollo.ts
+++ packages/mobil/src/apollo.ts
@@ -1,16 +1,16 @@
-import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient } from "apollo-client";
 import { createUploadLink } from "apollo-upload-client";
 import { Platform } from "react-native";
 
 const host =
-  Platform.OS === "ios"
-    ? "https://ed8b21400a63.ngrok.io/graphql"
-    : "http://10.0.2.2:4000";
+	Platform.OS === "ios"
+		? "https://ed8b21400a63.ngrok.io/graphql"
+		: "http://10.0.2.2:4000";
 
 export const client = new ApolloClient({
-  link: createUploadLink({
-    uri: host,
-  }),
-  cache: new InMemoryCache(),
+	link: createUploadLink({
+		uri: host,
+	}),
+	cache: new InMemoryCache(),
 });
