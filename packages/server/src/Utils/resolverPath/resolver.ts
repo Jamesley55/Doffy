cce4f924dae9162c9679dc0c71bcf28a2e8d705f
Jamesley <Jamesleyjoseph5@gmail.com>
2020-07-28 18:20:52 -0400

    working on Booking pages

--- packages/server/src/Utils/resolverPath/resolver.ts
+++ packages/server/src/Utils/resolverPath/resolver.ts
@@ -1,47 +1,49 @@
-import { changePassword } from "./../../modules/Password/changePassword/resolvers";
 import { loginResolver } from "../../modules/Auth/login/resolvers";
+import { Logout } from "../../modules/Auth/logout/resolvers";
+import { Me } from "../../modules/Auth/me/resolvers";
 import { registerResolver } from "../../modules/Auth/register/resolvers";
-import { forgotPassword } from "../../modules/Password/forgotPassword/resolvers";
-import { fileUpload } from "../../modules/Pictures/uploadS3/resolvers";
-import { createService } from "../../modules/Services/create/resolvers";
+import { createBooking } from "../../modules/Booking/Create/resolvers";
+import { QueryBooking } from "../../modules/Booking/QueryBooking/resolvers";
+import { BookingTime } from "../../modules/Booking/QuerybookingTime/bookingtime";
+import { UpdateBooking } from "../../modules/Booking/update/resolvers";
+import { findServiceCalendar } from "../../modules/Calendar/QueryCalendar/resolvers";
 import { createMessage } from "../../modules/Message/create/resolver";
 import { newMessage } from "../../modules/Message/newMessage/resolvers";
-import { searchServicesUser } from "../../modules/search/resolvers";
 import { findMessage } from "../../modules/Message/QueryMessage/resolvers";
-import { Me } from "../../modules/Auth/me/resolvers";
-import { Logout } from "../../modules/Auth/logout/resolvers";
-import { ServiceByCategory } from "../../modules/Services/QueryServices/resolver";
-import { DeleteService } from "../../modules/Services/delete/resolvers";
-import { UpdateService } from "../../modules/Services/update/resolvers";
-import { findServiceCalendar } from "../../modules/Calendar/QueryCalendar/resolvers";
 import { createNotification } from "../../modules/Notification/create/resolver";
 import { newNotification } from "../../modules/Notification/newNotification/resolvers";
 import { findNotification } from "../../modules/Notification/Querynotification/resolvers";
-import { createBooking } from "../../modules/Booking/Create/resolvers";
-import { UpdateBooking } from "../../modules/Booking/update/resolvers";
-import { QueryBooking } from "../../modules/Booking/QueryBooking/resolvers";
+import { forgotPassword } from "../../modules/Password/forgotPassword/resolvers";
+import { fileUpload } from "../../modules/Pictures/uploadS3/resolvers";
+import { searchServicesUser } from "../../modules/search/resolvers";
+import { createService } from "../../modules/Services/create/resolvers";
+import { DeleteService } from "../../modules/Services/delete/resolvers";
+import { ServiceByCategory } from "../../modules/Services/QueryServices/resolver";
+import { UpdateService } from "../../modules/Services/update/resolvers";
+import { changePassword } from "./../../modules/Password/changePassword/resolvers";
 
 export const resolvers = [
-  loginResolver,
-  registerResolver,
-  forgotPassword,
-  changePassword,
-  fileUpload,
-  createService,
-  createMessage,
-  findMessage,
-  newMessage,
-  searchServicesUser,
-  Me,
-  Logout,
-  ServiceByCategory,
-  DeleteService,
-  UpdateService,
-  findServiceCalendar,
-  createNotification,
-  newNotification,
-  findNotification,
-  createBooking,
-  UpdateBooking,
-  QueryBooking,
+	loginResolver,
+	registerResolver,
+	forgotPassword,
+	changePassword,
+	fileUpload,
+	createService,
+	createMessage,
+	findMessage,
+	newMessage,
+	searchServicesUser,
+	Me,
+	Logout,
+	ServiceByCategory,
+	DeleteService,
+	UpdateService,
+	findServiceCalendar,
+	createNotification,
+	newNotification,
+	findNotification,
+	createBooking,
+	UpdateBooking,
+	QueryBooking,
+	BookingTime,
 ];
---
+++ packages/server/src/modules/Booking/QuerybookingTime/bookingtime.ts
@@ -0,0 +1,67 @@
+import { IResolvers } from "apollo-server-express";
+import { Booking } from "../../../entity/booking";
+import { Calendar } from "../../../entity/calendar";
+import { Service } from "../../../entity/service";
+import { ScheduleTime } from "../../../entity/times";
+import { getHours } from "../../../Sharefonction/milisecondTohours";
+import { getWeekDay } from "../../../Sharefonction/wekkday";
+export const BookingTime: IResolvers = {
+	Query: {
+		QueryBookingTime: async (_, { serviceId, date }) => {
+			const service = await Service.findOne({ where: { id: serviceId } });
+			console.log(service);
+			const calendar = await Calendar.findOne({
+				where: { CalendarId: service?.calendarId },
+			});
+			const hours: string[] = [];
+			let endTime: number = 0;
+			const day: any = new Date(date);
+			const weekDay = getWeekDay(day);
+			let scheduleTimeId = null;
+			weekDay === "Sunday"
+				? (scheduleTimeId = calendar?.sundayScheduleUuid)
+				: weekDay === "Monday"
+				? (scheduleTimeId = calendar?.mondayScheduleUuid)
+				: weekDay === "Tuesday"
+				? (scheduleTimeId = calendar?.tuesdayScheduleUuid)
+				: weekDay === "Wednesday"
+				? (scheduleTimeId = calendar?.wednesdayScheduleUuid)
+				: weekDay === "Thursday"
+				? (scheduleTimeId = calendar?.thurdayScheduleUuid)
+				: weekDay === "Friday"
+				? (scheduleTimeId = calendar?.fridayScheduleUuid)
+				: weekDay === "Saturday"
+				? (scheduleTimeId = calendar?.saturdayScheduleUuid)
+				: null;
+
+			const schedule = await ScheduleTime.findOne({
+				where: { scheduleTimeId },
+			});
+
+			console.log(schedule);
+			if (schedule?.EndTime) {
+				endTime = schedule.EndTime;
+			}
+			for (
+				let i: any = schedule?.startingTime;
+				i < endTime;
+				i += service?.averageTime
+			) {
+				const booking = await Booking.findOne({
+					where: {
+						serviceId,
+						date,
+						startService: i,
+						endService: i + service?.averageTime,
+					},
+				});
+				if (!booking) {
+					const h: string = getHours(i);
+					hours.push(h);
+				}
+			}
+
+			return hours;
+		},
+	},
+};
